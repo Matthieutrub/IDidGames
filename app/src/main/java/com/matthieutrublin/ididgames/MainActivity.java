ab66041b4bdc1c0b7df14bd6a626cab62b5ca96f
Matthieutrub <matthieutrub@outlook.com>
2017-12-28 11:44:47 +0100

    cleaning code

--- app/src/main/java/com/matthieutrublin/ididgames/MainActivity.java
+++ app/src/main/java/com/matthieutrublin/ididgames/MainActivity.java
@@ -1,8 +1,8 @@
 package com.matthieutrublin.ididgames;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
@@ -10,19 +10,19 @@ import android.widget.TableLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editTextTableWidth = null;
-    EditText editTextTableHeight = null;
-    TableLayout matrice =null;
-    Context contextThis = null;
+    Context contextThis;
+    EditText editTextTableWidth;
+    EditText editTextTableHeight;
+    TableLayout matrice;
     TextView nbForm;
     TextView textViewPerimeter;
     Matrice myMatrice;
 
     private void initTable() {
 
-        Integer tableWidth=Integer.parseInt(editTextTableWidth.getText().toString());
-        Integer tableHeight=Integer.parseInt(editTextTableHeight.getText().toString());
-        myMatrice=new Matrice(contextThis,tableWidth,tableHeight,matrice,textViewPerimeter);
+        Integer tableWidth = Integer.parseInt(editTextTableWidth.getText().toString());
+        Integer tableHeight = Integer.parseInt(editTextTableHeight.getText().toString());
+        myMatrice = new Matrice(contextThis, tableWidth, tableHeight, matrice, textViewPerimeter);
 
     }
 
@@ -46,31 +46,38 @@ public class MainActivity extends AppCompatActivity {
 
         editTextTableWidth.addTextChangedListener(new TextWatcher() {
             public void afterTextChanged(Editable s) {
-                if(editTextTableWidth.getText().toString().isEmpty()){
-                } else if(Integer.parseInt(editTextTableWidth.getText().toString())>25){
+                if (editTextTableWidth.getText().toString().isEmpty()) {
+                } else if (Integer.parseInt(editTextTableWidth.getText().toString()) > 25) {
                     editTextTableWidth.setText("25");
                     initTable();
                 } else {
                     initTable();
                 }
             }
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
         });
         editTextTableHeight.addTextChangedListener(new TextWatcher() {
             public void afterTextChanged(Editable s) {
-                if(editTextTableHeight.getText().toString().isEmpty()){
-                } else if(Integer.parseInt(editTextTableHeight.getText().toString())>25){
+                if (editTextTableHeight.getText().toString().isEmpty()) {
+                } else if (Integer.parseInt(editTextTableHeight.getText().toString()) > 25) {
                     editTextTableHeight.setText("25");
                     initTable();
                 } else {
                     initTable();
                 }
             }
+
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
         });
 
 
@@ -80,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         outState.putInt("height", myMatrice.getHeight());
         outState.putInt("width", myMatrice.getWidth());
-        for(int i = 0; i < myMatrice.getHeight(); i++) {
+        for (int i = 0; i < myMatrice.getHeight(); i++) {
             outState.putBooleanArray("MatriceBool" + i, myMatrice.getMatriceBool()[i]);
         }
         super.onSaveInstanceState(outState);
@@ -89,14 +96,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        int height = savedInstanceState.getInt("height",10);
-        int width = savedInstanceState.getInt("width",10);
+        int height = savedInstanceState.getInt("height", 10);
+        int width = savedInstanceState.getInt("width", 10);
         boolean table[][] = new boolean[25][25];
-        for(int i = 0; i < height; i++) {
-            table[i]=savedInstanceState.getBooleanArray("MatriceBool" + i);
+        for (int i = 0; i < height; i++) {
+            table[i] = savedInstanceState.getBooleanArray("MatriceBool" + i);
         }
 
-        myMatrice=new Matrice(contextThis,width,height,matrice,textViewPerimeter);
+        myMatrice = new Matrice(contextThis, width, height, matrice, textViewPerimeter);
         myMatrice.setMatriceValue(table);
         myMatrice.updateColorFromMatrice();
         myMatrice.updateData();
--- app/src/main/java/com/matthieutrublin/ididgames/Matrice.java
+++ app/src/main/java/com/matthieutrublin/ididgames/Matrice.java
@@ -14,143 +14,138 @@ import android.widget.TextView;
 
 public class Matrice {
 
-    Context context;
-    TableLayout table;
-    int width;
-    int height;
-    TextView pixel[][]=new TextView[25][25];
-    boolean matriceBool[][]=new boolean[25][25];
-    Integer colorPlain = Color.argb(255, 0, 175, 0);
-    Integer colorEmpty = Color.argb(255, 175, 64, 64);
-    int perimeter=0;
-    TextView textViewPerimeter;
-
-    public int getPerimeter(){
-        return perimeter;
-    }
-
-    public int getHeight(){
-        return height;
-    }
-
-    public int getWidth(){
-        return width;
-    }
-
-    public boolean[][] getMatriceBool(){
-        return matriceBool;
-    }
-
-    public void setMatriceValue(boolean[][] data){
-        matriceBool=data;
-    }
-
-
-    public Matrice(Context fromContext, int x, int y, TableLayout tableLayout, TextView tvPerimeter)
-    {
-        context=fromContext;
-        width=x;
-        height=y;
-        table=tableLayout;
-        textViewPerimeter=tvPerimeter;
-        table.removeAllViews();
-        textViewPerimeter.setText("0");
-        for(int i = 0; i < height; i++){
-            TableRow row=new TableRow(context);
-            for(int j = 0; j < width; j++)
-            {
-                matriceBool[i][j]=false;
-                //matriceForm[i][j]=0;
-                pixel[i][j] =new TextView(context);
-                pixel[i][j].setText("    ");
-                pixel[i][j].setClickable(true);
-                pixel[i][j].setBackgroundColor(colorEmpty);
-                pixel[i][j].setOnClickListener(clickListenerPixel);
-                row.addView(pixel[i][j]);
-            }
-            table.addView(row,i);
-        }
-    }
+    private final static Integer COLORPLAIN = Color.argb(255, 0, 175, 0);
+    private final static Integer COLOREMPTY = Color.argb(255, 175, 64, 64);
+    private Context context;
+    private TableLayout table;
+    private int width;
+    private int height;
+    private TextView pixel[][] = new TextView[25][25];
+    private boolean matriceBool[][] = new boolean[25][25];
+    private int perimeter;
+    private TextView textViewPerimeter;
 
     private View.OnClickListener clickListenerPixel = new View.OnClickListener() {
 
         @Override
         public void onClick(View v) {
             ColorDrawable pixelColor = (ColorDrawable) v.getBackground();
-            if(pixelColor.getColor()==colorPlain){
-                v.setBackgroundColor(colorEmpty);
+            if (pixelColor.getColor() == COLORPLAIN) {
+                v.setBackgroundColor(COLOREMPTY);
             } else {
-                v.setBackgroundColor(colorPlain);
+                v.setBackgroundColor(COLORPLAIN);
             }
             updateMatriceFromColor();
             updateData();
         }
     };
 
+    public Matrice(Context fromContext, int x, int y, TableLayout tableLayout, TextView tvPerimeter) {
+        context = fromContext;
+        width = x;
+        height = y;
+        table = tableLayout;
+        textViewPerimeter = tvPerimeter;
+
+        perimeter=0;
+        table.removeAllViews();
+        textViewPerimeter.setText("0");
+        for (int i = 0; i < height; i++) {
+            TableRow row = new TableRow(context);
+            for (int j = 0; j < width; j++) {
+                matriceBool[i][j] = false;
+                pixel[i][j] = new TextView(context);
+                pixel[i][j].setText("    ");
+                pixel[i][j].setClickable(true);
+                pixel[i][j].setBackgroundColor(COLOREMPTY);
+                pixel[i][j].setOnClickListener(clickListenerPixel);
+                row.addView(pixel[i][j]);
+            }
+            table.addView(row, i);
+        }
+    }
+
+    public int getPerimeter() {
+        return perimeter;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public boolean[][] getMatriceBool() {
+        return matriceBool;
+    }
+
+    public void setMatriceValue(boolean[][] data) {
+        matriceBool = data;
+    }
+
     private void updateMatriceFromColor() {
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++)
-            {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 ColorDrawable pixelColor = (ColorDrawable) pixel[i][j].getBackground();
-                if(pixelColor.getColor()==colorPlain){
-                    matriceBool[i][j]=true;
+                if (pixelColor.getColor() == COLORPLAIN) {
+                    matriceBool[i][j] = true;
                 } else {
-                    matriceBool[i][j]=false;
+                    matriceBool[i][j] = false;
                 }
             }
         }
     }
 
     public void updateColorFromMatrice() {
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++)
-            {
-                if(matriceBool[i][j]){
-                    pixel[i][j].setBackgroundColor(colorPlain);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matriceBool[i][j]) {
+                    pixel[i][j].setBackgroundColor(COLORPLAIN);
                 } else {
-                    pixel[i][j].setBackgroundColor(colorEmpty);
+                    pixel[i][j].setBackgroundColor(COLOREMPTY);
                 }
             }
         }
     }
 
     public void updateData() {
-        perimeter=0;
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++)
-            {
-                if(matriceBool[i][j]){
+        perimeter = 0;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matriceBool[i][j]) {
 
                     //Up
-                    if(i==0){
+                    if (i == 0) {
                         perimeter++;
-                    } else if (!matriceBool[i-1][j]){
+                    } else if (!matriceBool[i - 1][j]) {
                         perimeter++;
                     }
 
                     //Down
-                    if(i+1==height){
+                    if (i + 1 == height) {
                         perimeter++;
-                    } else if (!matriceBool[i+1][j]){
+                    } else if (!matriceBool[i + 1][j]) {
                         perimeter++;
                     }
                     //Left
-                    if(j==0){
+                    if (j == 0) {
                         perimeter++;
-                    } else if (!matriceBool[i][j-1]){
+                    } else if (!matriceBool[i][j - 1]) {
                         perimeter++;
                     }
 
                     //Right
-                    if(j+1==width){
+                    if (j + 1 == width) {
                         perimeter++;
-                    } else if (!matriceBool[i][j+1]){
+                    } else if (!matriceBool[i][j + 1]) {
                         perimeter++;
                     }
                 }
             }
         }
-        textViewPerimeter.setText(perimeter+" ");
+        textViewPerimeter.setText(perimeter + " ");
     }
 
 }
